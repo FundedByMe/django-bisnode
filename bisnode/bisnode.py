c748a2ab61a4ac92f313e34331b04fcb1b54cec6
Ana Hristova <ana@fundedbyme.com>
2014-10-16 16:03:29 +0200

    Attempt to fix tests

--- bisnode/bisnode.py
+++ bisnode/bisnode.py
@@ -3,20 +3,22 @@ from django.conf import settings
 from suds.client import Client
 
 
-bisnode_soap_headers = {
-    'userPassword': settings.BISNODE_USER_PASSWORD,
-    'userId': settings.BISNODE_USER_ID,
-    'customerCode': settings.BISNODE_CUSTOMER_CODE,
-    'customerCodeOwner': settings.BISNODE_CUSTOMER_OWNER,
-    'language': settings.BISNODE_DEFAULT_LANGUAGE,
-    'fromCountry': settings.BISNODE_DEFAULT_FROM_COUNTRY,
-    'toCountry': settings.BISNODE_DEFAULT_TO_COUNTRY}
+def get_bisnode_soap_headers():
+    bisnode_soap_headers = {
+        'userPassword': settings.BISNODE_USER_PASSWORD,
+        'userId': settings.BISNODE_USER_ID,
+        'customerCode': settings.BISNODE_CUSTOMER_CODE,
+        'customerCodeOwner': settings.BISNODE_CUSTOMER_OWNER,
+        'language': settings.BISNODE_DEFAULT_LANGUAGE,
+        'fromCountry': settings.BISNODE_DEFAULT_FROM_COUNTRY,
+        'toCountry': settings.BISNODE_DEFAULT_TO_COUNTRY}
+    return bisnode_soap_headers
 
 
 def get_bisnode_api_client(service_name):
     url = "https://www.bisgateway.com/brg/services/%s?wsdl" % service_name
     client = Client(url)
-    client.set_options(soapheaders=bisnode_soap_headers)
+    client.set_options(soapheaders=get_bisnode_soap_headers())
     return client
 
 
