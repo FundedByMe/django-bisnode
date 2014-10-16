2e3596714c4053a785bb64a8c567861adfa67dc0
Ana Hristova <ana@fundedbyme.com>
2014-10-16 13:56:35 +0200

    Add Bisnode Rating Report factory

---
+++ bisnode/tests/factories.py
@@ -0,0 +1,14 @@
+from django.utils.timezone import now
+
+import factory
+
+from ..models import BisnodeRatingReport
+
+
+class BisnodeRatingReportFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = BisnodeRatingReport
+
+    organization_number = "5561234567"
+    rating = None
+    date_of_rating = None
+    last_updated = now()
