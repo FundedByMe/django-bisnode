e3544de81cbaf859d341102d79a19e8239eef768
Ana Hristova <ana@fundedbyme.com>
2014-10-21 16:04:13 +0200

    Start adding Bisnode Standard report

--- bisnode/constants.py
+++ bisnode/constants.py
@@ -10,11 +10,97 @@ BAD = 'C'
 MISSING = '-'
 
 RATING_CHOICES = (
-    (HIGH, "high"),
-    (GOOD, "good"),
-    (WORTHY, "worthy"),
-    (NEW, "new"),
-    (BELOW_AVERAGE, "below average"),
-    (BAD, "bad"),
-    (MISSING, "missing"),
+    (HIGH, "Highest credit worthiness"),
+    (GOOD, "Good credit worthiness"),
+    (WORTHY, "Credit worthy"),
+    (NEW, "New company"),
+    (BELOW_AVERAGE, "Credit with safety measurements"),
+    (BAD, "Credit advised against"),
+    (MISSING, "Rating not applicable"),
 )
+
+
+WELL_ESTABLISHED = 'AGE010'
+ESTABLISHED = 'AGE020'
+SMALLER_SCALE = 'AGE030'
+LESS_THAN_TWO_YEARS = 'AGE040'
+
+OPERATION_CHOICES = (
+    (WELL_ESTABLISHED, 'Well-established'),
+    (ESTABLISHED, 'Established'),
+    (SMALLER_SCALE, '2–4 years'),
+    (LESS_THAN_TWO_YEARS, 'Less than two years'),
+)
+
+OPERATION_CHOICES_DICT = dict(OPERATION_CHOICES)
+
+
+SATISFACTORY = 'OWN030'
+NOTES_ON_RECORD = 'OWN100'
+NEGATIVE_ON_RECORD = 'OWN040'
+CURRENT_NEGATIVE = 'OWN050'
+NEGATIVE_GROUP = 'OWN090'
+INCOMPLETE = 'OWN080'
+
+MANAGEMENT_CHOICES = (
+    (SATISFACTORY, 'Satisfactory'),
+    (NOTES_ON_RECORD, 'Notes on record'),
+    (NEGATIVE_ON_RECORD, 'Negative info on record'),
+    (NEGATIVE_GROUP, 'Negative info – group'),
+    (CURRENT_NEGATIVE, 'Current negative info'),
+    (INCOMPLETE, 'Incomplete')
+)
+
+MANAGEMENT_CHOICES_DICT = dict(MANAGEMENT_CHOICES)
+
+
+VERY_GOOD_FINANCES = 'ECO010'
+GOOD_FINANCES = 'ECO020'
+SATISFACTORY_FINANCES = 'ECO030'
+UNSATISFACTORY_FINANCES = 'ECO040'
+WEAK_FINANCES = 'ECO050'
+SEE_SPECIFICATIONS = 'ECO060'
+NOT_UPDATED = 'ECO070'
+NOT_UPDATED2 = 'ECO090'
+NOT_UPDATED3 = 'ECO100'
+UNAVAILABLE = 'ECO110'
+
+FINANCES_CHOICES = (
+    (VERY_GOOD_FINANCES, 'Very good'),
+    (GOOD_FINANCES, 'Good'),
+    (SATISFACTORY_FINANCES, 'Satisfactory'),
+    (UNSATISFACTORY_FINANCES, 'Unsatisfactory'),
+    (WEAK_FINANCES, 'Weak'),
+    (SEE_SPECIFICATIONS, 'See financial specifications'),
+    (NOT_UPDATED, 'Annual accounts not updated'),
+    (NOT_UPDATED2, 'Annual accounts not updated'),
+    (NOT_UPDATED3, 'Annual accounts not updated'),
+    (UNAVAILABLE, 'Annual accounts unavailable')
+)
+
+FINANCES_CHOICES_DICT = dict(FINANCES_CHOICES)
+
+
+VERY_GOOD_SOLVENCY = 'PAY010'
+VERY_GOOD_NOTES_ON_RECORD = 'PAY020'
+GOOD_SOLVENCY = 'PAY030'
+GOOD_NOTES_ON_RECORD = 'PAY040'
+SATISFACTORY_SOLVENCY = 'PAY050'
+SATISFACTORY_NOTES_ON_RECORD = 'PAY060'
+UNSATISFACTORY_SOLVENCY = 'PAY070'
+WEAK_SOLVENCY = 'PAY080'
+VERY_WEAK_SOLVENCY = 'PAY090'
+
+SOLVENCY_CHOICES = (
+    (VERY_GOOD_SOLVENCY, 'Very good'),
+    (VERY_GOOD_NOTES_ON_RECORD, 'Very good – notes on record'),
+    (GOOD_SOLVENCY, 'Good'),
+    (GOOD_NOTES_ON_RECORD, 'Good – notes on record'),
+    (SATISFACTORY_SOLVENCY, 'Satisfactory'),
+    (SATISFACTORY_NOTES_ON_RECORD, 'Satisfactory – notes on record'),
+    (UNSATISFACTORY_SOLVENCY, 'Unsatisfactory'),
+    (WEAK_SOLVENCY, 'Weak'),
+    (VERY_WEAK_SOLVENCY, 'Very weak'),
+)
+
+SOLVENCY_CHOICES_DICT = dict(SOLVENCY_CHOICES)
--- bisnode/models.py
+++ bisnode/models.py
@@ -2,7 +2,9 @@ from datetime import datetime
 
 from django.db import models
 
-from .constants import COMPANY_RATING_REPORT, RATING_CHOICES
+from .constants import (COMPANY_RATING_REPORT, COMPANY_STANDARD_REPORT,
+                        RATING_CHOICES, OPERATION_CHOICES, MANAGEMENT_CHOICES,
+                        FINANCES_CHOICES, SOLVENCY_CHOICES)
 from .bisnode import get_bisnode_company_report
 
 
@@ -11,13 +13,20 @@ def bisnode_date_to_date(bisnode_date):
     return formatted_datetime.date()
 
 
-class BisnodeRatingReport(models.Model):
+class BinodeReport(models.Model):
+
     organization_number = models.CharField(max_length=10, unique=True)
     rating = models.CharField(max_length=3, choices=RATING_CHOICES,
                               null=True, blank=True)
     date_of_rating = models.DateField(blank=True, null=True)
     registration_date = models.DateField(blank=True, null=True)
     last_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class BisnodeRatingReport(BinodeReport):
 
     def get(self):
         rating_report = get_bisnode_company_report(
@@ -29,4 +38,34 @@ class BisnodeRatingReport(models.Model):
             company_data['dateOfRating'])
         self.registration_date = bisnode_date_to_date(
             company_data['dateReg'])
+        self.save()
+
+
+class BisnodeStandardReport(BinodeReport):
+    history_and_operation = models.CharField(
+        max_length=6, choices=OPERATION_CHOICES, blank=True)
+    management = models.CharField(
+        max_length=6, choices=MANAGEMENT_CHOICES, blank=True)
+    finances = models.CharField(
+        max_length=6, choices=FINANCES_CHOICES, blank=True)
+    solvency = models.CharField(
+        max_length=6, choices=SOLVENCY_CHOICES, blank=True)
+
+    number_of_employees = models.IntegerField(null=True, blank=True)
+
+    def get(self):
+        rating_report = get_bisnode_company_report(
+            report_type=COMPANY_STANDARD_REPORT,
+            organization_number=self.organization_number)
+        company_data = rating_report.generalCompanyData[0]
+        self.rating = company_data['ratingCode']
+        self.date_of_rating = bisnode_date_to_date(
+            company_data['dateOfRating'])
+        self.registration_date = bisnode_date_to_date(
+            company_data['dateReg'])
+        self.history_and_operation = company_data['historyCode']
+        self.management = company_data['shareholdersCode']
+        self.finances = company_data['financeCode']
+        self.solvency = company_data['abilityToPay1']
+        self.number_of_employees = company_data['noOfEmployees1']
         self.save()
