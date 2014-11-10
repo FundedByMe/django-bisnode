5881006c2dd67708fede5e645fda6e0e50617ecf
Ana Hristova <ana@fundedbyme.com>
2014-11-10 09:23:33 +0100

    Test create report with incorrect input

--- bisnode/tests/bisnode.py
+++ bisnode/tests/bisnode.py
@@ -4,22 +4,29 @@ from suds.client import Client
 
 from ..constants import COMPANY_STANDARD_REPORT, COMPANY_RATING_REPORT
 
+REPORTS_TEST_FILES_DICT = {
+    COMPANY_STANDARD_REPORT: 'standard_report.xml',
+    COMPANY_RATING_REPORT: 'rating_report.xml',
+}
 
-def get_bisnode_report_from_file(report_type, file_name):
+TEST_ORGANIZATION_NUMBER = "5561234567"
+
+
+def get_test_bisnode_response_message(file_name):
+    file_path = os.path.join(os.path.dirname(__file__), 'data', file_name)
+    with open(file_path, "r") as message_file:
+        response_message = message_file.read()
+    return response_message
+
+
+def mock_get_bisnode_company_report(report_type, organization_number):
     url = "https://www.bisgateway.com/brg/services/%s?wsdl" % report_type
     client = Client(url)
-    file_path = os.path.join(os.path.dirname(__file__), file_name)
-    with open(file_path, "r") as response_message_file:
-        response_message = response_message_file.read()
-    report = client.service.service(__inject={'reply': response_message})
-    return report
-
-
-def get_bisnode_test_standard_report():
-    return get_bisnode_report_from_file(COMPANY_STANDARD_REPORT,
-                                        'data/standard_report.xml')
-
-
-def get_bisnode_test_rating_report():
-    return get_bisnode_report_from_file(COMPANY_RATING_REPORT,
-                                        'data/rating_report.xml')
+    if organization_number == TEST_ORGANIZATION_NUMBER:
+        report_file_name = REPORTS_TEST_FILES_DICT[report_type]
+        response_message = get_test_bisnode_response_message(report_file_name)
+        message_type = 'reply'
+    else:
+        response_message = get_test_bisnode_response_message("error.xml")
+        message_type = 'fault'
+    return client.service.service(__inject={message_type: response_message})
--- bisnode/tests/company_report.py
+++ bisnode/tests/company_report.py
@@ -1,12 +1,14 @@
 from django.test import TestCase
 
+from suds.client import WebFault
 from mock import patch
 from money import Money
 
 from ..models import BisnodeCompanyReport
-from ..constants import HIGH, GOOD_FINANCES
+from ..constants import (HIGH, GOOD_FINANCES, COMPANY_STANDARD_REPORT,
+                         COMPANY_RATING_REPORT)
 
-from .bisnode import get_bisnode_test_standard_report
+from .bisnode import mock_get_bisnode_company_report, TEST_ORGANIZATION_NUMBER
 from .factories import BisnodeCompanyReportFactory
 
 
@@ -15,7 +17,6 @@ class BisnodeCompanyReportTests(TestCase):
     def setUp(self):
         super(BisnodeCompanyReportTests, self).setUp()
         self.company_report = BisnodeCompanyReportFactory()
-        self.organization_number = "5561234567"
         self.number_of_employees = 6
         self.share_capital = Money(3000000.00, 'SEK')
 
@@ -35,15 +36,32 @@ class BisnodeCompanyReportTests(TestCase):
         self.assertEquals(self.company_report.share_capital, self.share_capital)
 
     @patch("bisnode.models.get_bisnode_company_report")
-    def test_create_rating_report(self, mock_report):
-        mock_report.return_value = get_bisnode_test_standard_report()
+    def test_create_rating_report_with_incorrect_input(self, mock_get_report):
+        mock_get_report.side_effect = mock_get_bisnode_company_report
+        with self.assertRaises(WebFault) as context:
+            self.company_report.create_standard_report("000000000")
+        self.assertEqual(context.exception.message,
+                         "Server raised fault: 'Error in NRG Transaction; "
+                         "NRG error code: 532; NRG error message: "
+                         "NOT VALID COMPANY'")
         self.assert_general_company_data_does_not_exist()
-        self.company_report.create_rating_report(self.organization_number)
+
+    @patch("bisnode.models.get_bisnode_company_report")
+    def test_create_rating_report(self, mock_get_report):
+        mock_get_report.side_effect = mock_get_bisnode_company_report
+        self.assert_general_company_data_does_not_exist()
+        self.company_report.create_rating_report(TEST_ORGANIZATION_NUMBER)
+        mock_get_report.assert_called_with(
+            report_type=COMPANY_RATING_REPORT,
+            organization_number=TEST_ORGANIZATION_NUMBER)
         self.assert_general_company_data_saved_successfully()
 
     @patch("bisnode.models.get_bisnode_company_report")
-    def test_create_standard_report(self, mock_report):
-        mock_report.return_value = get_bisnode_test_standard_report()
+    def test_create_standard_report(self, mock_get_report):
+        mock_get_report.side_effect = mock_get_bisnode_company_report
         self.assert_general_company_data_does_not_exist()
-        self.company_report.create_standard_report(self.organization_number)
+        self.company_report.create_standard_report(TEST_ORGANIZATION_NUMBER)
+        mock_get_report.assert_called_with(
+            report_type=COMPANY_STANDARD_REPORT,
+            organization_number=TEST_ORGANIZATION_NUMBER)
         self.assert_general_company_data_saved_successfully()
